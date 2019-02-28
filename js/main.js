b28fea1ce1f72fd0c255db5ae3d93252c4468fa0
oscar lopez deleyja <33075979+Os24@users.noreply.github.com>
2019-02-27 18:27:49 -0600

    some commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var botellas = [1,2,3]
-var totalOrigen = "";
-var cantidadBoteUno = "";
-var cantidadBoteDos = "";
-var cantidadBoteTres = "";
+var totalOrigen = 0;
+var cantidadBoteUno = 0;
+var cantidadBoteDos = 0;
+var cantidadBoteTres = 0;
 var cantidadBoteCinco = "";
 var totalOrigen = "";
 
@@ -27,12 +27,14 @@ function botellasCalc(){
     				cantidadBoteTres = Math.trunc(temp)
     				$("#botellas-display").html("botellas de 3 litros:  "+ cantidadBoteUno+ "<br>botellas de 2 litros:"+ cantidadBoteDos+ "<br>botellas de 1 litro:"+ cantidadBoteTres)
     			}
-
-    		}
-
-    	}
-
-	}
+            }
+        }
+    }else 
+    
+    if(totalOrigen % botellas[2]===0){
+        cantidadBoteUno = Math.trunc(totalOrigen/botellas[2])
+        $("#botellas-display").html("botellas de 3 litros:  "+ cantidadBoteUno+ "<br>botellas de 2 litros:"+ cantidadBoteDos)
+    }
 }
 
 
